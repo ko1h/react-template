ced365479ed0d1fe3e616aa629e3de33ffbdaf0b
Student <example@example.com>
2019-04-25 10:12:55 -0700

    Conditional Rendering

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
 
 class NewTicketControl extends React.Component {
 
@@ -7,17 +9,24 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-    console.log('Hey, you clicked me! I do not have code to change my state quite yet, but I will in a moment!');
-  }
+  // handleClick(){
+  //   this.setState({formVisibleOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  // }
 
   render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTicketForm />;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions />;
+    }
     return (
       <div>
-        <p>This is the NewTicketControl component!</p>
-        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+        {currentlyVisibleContent}
       </div>
     );
   }
