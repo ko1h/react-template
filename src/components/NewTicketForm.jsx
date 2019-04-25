e91faad9d5c2fb20d75c3094625ea0d01f8619af
Student <example@example.com>
2019-04-25 11:49:22 -0700

    Forms and Refs

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 
 function NewTicketForm(){
-  return (
-    <div>
-      <form>
-        <input
-          type='text'
-          id='names'
-          placeholder='Pair Names'/>
-        <input
-          type='text'
-          id='location'
-          placeholder='Location'/>
-        <textarea
-          id='issue'
-          placeholder='Describe your issue.'/>
-        <button type='submit'>Help!</button>
-      </form>
-    </div>
-  );
-}
+  let _names = null;
+  let _location = null;
+  let _issue = null;
 
-export default NewTicketForm;
+  function handleNewTicketFormSubmission(event) {
+   event.preventDefault();
+   console.log(_names);
+   console.log(_location);
+   console.log(_issue);
+ }
+   return (
+     <div>
+       <form onSubmit={handleNewTicketFormSubmission}>
+         <input
+           type='text'
+           id='names'
+           placeholder='Pair Names'
+           ref={(input) => {_names = input;}}/>
+         <input
+           type='text'
+           id='location'
+           placeholder='Location'
+           ref={(input) => {_location = input;}}/>
+         <textarea
+           id='issue'
+           placeholder='Describe your issue.'
+           ref={(textarea) => {_issue = textarea;}}/>
+         <button type='submit'>Help!</button>
+       </form>
+     </div>
+   );
+ }
+
+ export default NewTicketForm;
